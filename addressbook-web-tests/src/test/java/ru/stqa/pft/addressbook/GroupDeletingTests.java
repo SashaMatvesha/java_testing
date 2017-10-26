ce7bde1411e25116b6326cd8d646c3a20811aee4
alexandra <alexandra020195@yandex.ru>
2017-10-26 18:46:16 +0300

    записан тест удаления групп, создан базавый класс и установено наследование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletingTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletingTests extends TestBase {
+  @Test
+  public void testGroupDeleting() {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+}
